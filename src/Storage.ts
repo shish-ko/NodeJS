ce97212eac8966b4d044c2640cd374c09257481d
shish-ko <shishko.dev@gamil.com>
2023-06-29 09:48:02 +0300

    feat: server

--- src/Storage.ts
+++ src/Storage.ts
@@ -1,24 +1,37 @@
 import {v4 as uuid, validate} from 'uuid';
 
-export class Storage{
-  storage: IUser[];
-  static checkUser (user: Omit<IUser, 'id'>) {
-    if(typeof user.username !== 'string' || typeof user.age !== 'number' || !Array.isArray(user.hobbies)) return;
+export class Storage extends Array<IUser>{
+
+  static checkPayload (user?: Partial<IUser>, numProperties = 3) {
+    if(!user) return;
+    if(typeof user.username !== 'string' || typeof user.age !== 'number' || !Array.isArray(user.hobbies) || Object.keys(user).length > numProperties) return;
     return true    
   }
   static checkUUID (payload: string) {
     return validate(payload)
   }
-  constructor(){
-    this.storage = [];
-  }
   add (user: Omit<IUser, 'id'>){
     const id = uuid()
-    this.storage.push({...user, id})
+    const newUser = {...user, id};
+    this.push(newUser);
+    return newUser;
   }  
+  getUser (userId: string){
+    const user = this.find((item) => item.id === userId);
+    return user;
+  }  
+  updateUser(userId: string, payload?: Partial<IUser>) {
+    const user = this.getUser(userId) as IUser;  // TODO
+    if(!Storage.checkPayload(Object.assign({}, user, payload), 4)) return
+    return Object.assign(user, payload);
+  }
+  deleteUser(userId: string) {
+    const index = this.findIndex((item) => item.id === userId);
+    this.splice(index, 1);
+  }
 }
 
-interface IUser {
+export interface IUser {
   id: string,
   username: string,
   age: number,
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,86 @@
 import http from 'node:http';
 import dotenv from 'dotenv';
-import { Storage } from './Storage';
+import { IUser, Storage } from './Storage';
 
-Storage.checkItem({username: "Nick", age: 28, hobbies: ['q']});
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-const predefinedUsers = {
-
+const predefinedUsers: Omit<IUser, 'id'> = {
+  username: 'Nick',
+  age: 3,
+  hobbies: ['qweqwe', 'asdasd']
 }
+
+const db = new Storage();
+db.add(predefinedUsers);
+
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.write('Hello')
-  res.end()
+  const res404 = () => {
+    res.statusCode = 404;
+    res.end('This endpoint doesn\'t exist');
+  }
+  const { method, url } = req;
+  const userId = url?.split('/')[3];
+  console.log(userId)
+  if (!url?.startsWith('/api/users') || url.split('/').length > 4) {
+    return res404();
+  }
+  if (!userId && (method === 'PUT' || method === 'DELETE')) return res404();
+  if (userId) {
+    if (!Storage.checkUUID(userId)) {
+      res.statusCode = 400;
+      res.end('Invalid userId');
+      return;
+    }
+    if (!db.getUser(userId)) {
+      res.statusCode = 404;
+      res.end('User with provided id doesn\'t exist');
+      return
+    }
+  }
+  let chunks: Uint8Array[] = [];
+  let body: undefined | Partial<IUser>;
+  req.on('data', (chunk) => chunks.push(chunk)).on('end', () => {
+    if (chunks.length) {
+      body = JSON.parse(Buffer.concat(chunks).toString());
+    }
+    switch (method) {
+      case 'GET':
+        res.setHeader('Content-type', 'application/json');
+        if (userId) {
+          res.end(JSON.stringify(db.getUser(userId)))
+        } else {
+          res.end(JSON.stringify(db))
+        }
+        break;
+      case 'POST':
+        if(!Storage.checkPayload(body)){
+          res.statusCode = 400;
+          return res.end('Provide all required info');
+        }
+        res.statusCode = 201;
+        res.setHeader('Content-type', 'application/json');
+        res.end(JSON.stringify(db.add(body as Omit<IUser, 'id'>)));        
+        break;
+      case 'DELETE':
+        res.statusCode = 204;
+        db.deleteUser(userId as string);
+        res.end();        
+        break;
+      case 'PUT':
+        const user = db.updateUser(userId as string, body)
+        if(!user) {
+          res.statusCode=400;
+          return res.end('Check provided info')
+        }
+        res.setHeader('Content-type', 'application/json');
+        res.end(JSON.stringify(user));        
+        break;
+      default:
+        break;
+    }
+  })
+  // res.end();
 });
 
 server.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
